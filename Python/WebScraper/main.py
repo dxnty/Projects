d5250b954053982903a59c4a7294d6eac558cb73
Carlo Persini <63673949+dxnty@users.noreply.github.com>
2024-03-18 16:37:56 +0100

    Update of main.py
    Adding more comments and fixing some lines

--- Python/WebScraper/main.py
+++ Python/WebScraper/main.py
@@ -4,17 +4,19 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-# [] Functions []
+#Functions
 
 def main():
-    print("Hello, I'm the main function of the Scraper file.\nIf you see me something is wrong. Run again")
+    titles = base_scraper("https://pypi.org/project/beautifulsoup4/","h1")
+    #paragraphs = base_scraper("https://pypi.org/project/beautifulsoup4/","p")
+    dataset_creator(titles)
+    #dataset_creator(paragraphs)
 
 
 # Receive a website URL and an HTML tag to look for and return the list with the desired informations
 def base_scraper(URL, HTML_TAG_condition):
-    website_url = URL
-    SERVERresponse = requests.get(website_url) #HTML code response received from the server (es. 200, 404, 301 etc.)
-    HTML_content = SERVERresponse.content #HTML webpage content (HTML tags, paragraph etc.)
+    SERVER_response = requests.get(URL) #HTML code response received from the server (es. 200, 404, 301 etc.)
+    HTML_content = SERVER_response.content #HTML webpage content (HTML tags, paragraph etc.)
     soup = BeautifulSoup(HTML_content, 'html.parser')
 
     return soup.find_all(HTML_TAG_condition)
@@ -26,7 +28,6 @@ def dataset_creator(list):
     #fetch and catalog the list received and fix the CSV syntax so that it's possible to correctly upload it into a csv dataset
 
 
-# [] Main function calls []
+#Main execution code
 if __name__ == "__main__":
-    titles = base_scraper("https://pypi.org/project/beautifulsoup4/","p")
-    dataset_creator(titles)
+    main()
